6a9b37c192fac4985378c3a108ddd55d0c1f7cf0
Nguyen, James <james.nguyen2+origin@originenergy.com.au>
2026-05-26 14:26:04 +1000

    fix: update explicit imports

--- src/main/java/james/expense_tracker/controller/ExpenseController.java
+++ src/main/java/james/expense_tracker/controller/ExpenseController.java
@@ -5,7 +5,13 @@ import java.util.List;
 import james.expense_tracker.model.ExpenseType;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 import james.expense_tracker.service.ExpenseService;
 import james.expense_tracker.dto.expense.CreateExpenseRequest;
