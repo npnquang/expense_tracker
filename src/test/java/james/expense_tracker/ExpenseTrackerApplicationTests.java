7870f20b51e242cd6d938148f23b85a3c2f9acdd
npnquang <nguyenjoe0201@gmail.com>
2026-06-11 18:52:30 +1000

    feat: add dynamic props to test

--- src/test/java/james/expense_tracker/ExpenseTrackerApplicationTests.java
+++ src/test/java/james/expense_tracker/ExpenseTrackerApplicationTests.java
@@ -2,9 +2,21 @@ package james.expense_tracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class ExpenseTrackerApplicationTests {
+    @DynamicPropertySource
+    static void jwtProps(DynamicPropertyRegistry registry) {
+        byte[] key = new byte[32];
+        new java.security.SecureRandom().nextBytes(key);
+        registry.add("jwt.secret", () -> java.util.Base64.getEncoder().encodeToString(key));
+        registry.add("jwt.access-expiration-ms", () -> "3600000");
+        registry.add("jwt.refresh-expiration-ms", () -> "86400000");
+    }
 
     @Test
     void contextLoads() {}
