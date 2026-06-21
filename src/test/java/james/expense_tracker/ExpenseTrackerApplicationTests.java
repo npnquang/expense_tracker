08b858f111a2469aeeb08520eebf0df1996f597a
npnquang <nguyenjoe0201@gmail.com>
2026-06-21 22:45:13 +1000

    fix: remove unused import

--- src/test/java/james/expense_tracker/ExpenseTrackerApplicationTests.java
+++ src/test/java/james/expense_tracker/ExpenseTrackerApplicationTests.java
@@ -2,7 +2,6 @@ package james.expense_tracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 
