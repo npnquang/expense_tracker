57f88cfe539d4c8a7fdddf4fd59db96bbd8332a3
npnquang <nguyenjoe0201@gmail.com>
2026-08-02 14:10:19 +1000

    feat: add index to table

--- sql/init/01-create-db.sql
+++ sql/init/01-create-db.sql
@@ -1,33 +1,37 @@
 DROP TABLE IF EXISTS expense CASCADE;
+
 DROP TYPE IF EXISTS expense_type;
 
-CREATE TYPE expense_type AS ENUM (
+CREATE TYPE expense_type AS ENUM(
     'FOOD',
     'TRANSPORT',
     'HOUSING',
     'UTILITIES',
     'OTHER'
-    );
+);
 
-CREATE TYPE user_role_enum AS ENUM (
+CREATE TYPE user_role_enum AS ENUM(
     'ADMIN',
     'USER'
-    );
+);
 
-CREATE TABLE user_info
-(
-    id        BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    username  VARCHAR(255)   NOT NULL UNIQUE,
-    email     VARCHAR(255)   NOT NULL UNIQUE,
-    password  VARCHAR(255)   NOT NULL,
+CREATE TABLE user_info (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
     user_role user_role_enum NOT NULL DEFAULT 'USER'
 );
 
-CREATE TABLE expense
-(
-    id          BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    description VARCHAR(255)   NOT NULL,
-    amount      NUMERIC(10, 2) NOT NULL,
-    type        expense_type   NOT NULL,
-    created_at  timestamptz    NOT NULL DEFAULT NOW()
+CREATE TABLE expense (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    description VARCHAR(255) NOT NULL,
+    amount NUMERIC(10, 2) NOT NULL,
+    type expense_type NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX expense_user_time ON expense (
+    user_id,
+    created_at
 );
