f44b33f0adc49fa0d8d98b77be3827c4237ef1c0
npnquang <nguyenjoe0201@gmail.com>
2026-08-03 16:08:22 +1000

    fix: add email to update password request and remove email as editable from update profile request

--- src/main/java/james/expense_tracker/dto/user/UpdatePasswordRequest.java
+++ src/main/java/james/expense_tracker/dto/user/UpdatePasswordRequest.java
@@ -1,10 +1,13 @@
 package james.expense_tracker.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record UpdatePasswordRequest(
+        // TODO: Implement
         @NotNull(message = "New password must not be empty")
                 @Size(min = 3, max = 50, message = "Password must be between 3 and 50 characters")
                 @Pattern(
@@ -18,4 +21,7 @@ public record UpdatePasswordRequest(
                         regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).+$",
                         message =
                                 "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character")
-                String oldPassword) {}
+                String oldPassword,
+        @NotBlank(message = "Email is required")
+                @Email
+                String email) {}
--- src/main/java/james/expense_tracker/dto/user/UpdateProfileRequest.java
+++ src/main/java/james/expense_tracker/dto/user/UpdateProfileRequest.java
@@ -1,6 +1,5 @@
 package james.expense_tracker.dto.user;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -9,6 +8,4 @@ public record UpdateProfileRequest(
         @Size(min = 3, max = 50, message = "New username must be between 3 and 50 characters")
                 @Pattern(regexp = "^[a-zA-Z0-9_]+$")
                 String newUserName,
-        @Size(min = 3, max = 50, message = "New email must be between 3 and 50 characters") @Email
-                String newEmail,
         @NotNull(message = "User ID must not be empty") Long userId) {}
